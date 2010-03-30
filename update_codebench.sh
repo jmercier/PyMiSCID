49901d376305363b2114aa364ce25223f9bab343
J-Pascal Mercier <jean-pascal.mercier@inrialpes.fr>
2010-03-30 11:08:11 +0200

    Add an update codebench script

---
+++ update_codebench.sh
@@ -0,0 +1,12 @@
+DIR="pymiscid/codebench"
+FILES="events.py wref.py xml.py generator.py"
+rm -rf ${DIR}
+mkdir ${DIR}
+for file in ${FILES}
+do
+    echo "copying ${file} from codebench"
+    cp ../codebench/${file} ${DIR}
+done
+touch ${DIR}/__init__.py
+
+
